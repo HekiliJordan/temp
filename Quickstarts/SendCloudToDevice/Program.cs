c41139e6a9c2845881ecd192cb8a8d5a2915676b
Hekili Jordan <hekili.jordan@wsu.edu>
2020-12-09 15:05:27 -0800

    init commit

---
+++ Quickstarts/SendCloudToDevice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace SendCloudToDevice
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=KeyHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=alcUDny8jMKl/+te/up3PjX0LDwKZK2ULxT69ksXNWc=";
+        static string targetDevice = "MyDotnetDevice";
+
+        private async static Task SendDeviceToCloudMessagesAsync(string input)
+        {
+            var commandMessage = new
+            Message(Encoding.ASCII.GetBytes(input));
+            commandMessage.Ack = DeliveryAcknowledgement.Full;
+            await serviceClient.SendAsync(targetDevice, commandMessage);
+        }
+
+        private async static void ReceiveFeedbackAsync()
+        {
+            var feedbackReceiver = serviceClient.GetFeedbackReceiver();
+
+            Console.WriteLine("\nReceiving c2d feedback from service");
+            while (true)
+            {
+                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                if (feedbackBatch == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received feedback: {0}",
+                  string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                Console.ResetColor();
+
+                await feedbackReceiver.CompleteAsync(feedbackBatch);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //int counter = 0;
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            while(true)
+            {
+                ReceiveFeedbackAsync();
+                Console.WriteLine("Press any key to send a message.");
+                string input = Console.ReadLine();
+                SendDeviceToCloudMessagesAsync(input).Wait();
+                //Console.ReadLine();
+                //counter++;
+            }
+           
+        }
+    }
+}
---
+++ Quickstarts/simulated-device/SimulatedDevice.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// This application uses the Azure IoT Hub device SDK for .NET
+// For samples see: https://github.com/Azure/azure-iot-sdk-csharp/tree/master/iothub/device/samples
+
+using System;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulated_device
+{
+    class SimulatedDevice
+    {
+        private static DeviceClient s_deviceClient;
+
+        // The device connection string to authenticate the device with your IoT hub.
+        // Using the Azure CLI:
+        // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
+        private readonly static string s_connectionString = "HostName=KeyHub.azure-devices.net;DeviceId=MyDotnetDevice;SharedAccessKey=Z/sVFO8LvJxQ+7RUuezeGtJiXjkO8QGIHS39tYz76LY=";
+
+        // Async method to send simulated telemetry
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+       
+                // Initial telemetry values
+                double minTemperature = 20;
+                double minHumidity = 60;
+                Random rand = new Random();
+
+                while (true)
+                {
+                    double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                    double currentHumidity = minHumidity + rand.NextDouble() * 20;
+                    string test = "Hello world";
+
+                    // Create JSON message
+                    var telemetryDataPoint = new
+                    {
+
+                        temperature = currentTemperature,
+                        humidity = currentHumidity,
+                        hw = test
+                    };
+                    var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                    var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                    // Add a custom application property to the message.
+                    // An IoT hub can filter on these properties without access to the message body.
+                    message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                    // Send the telemetry message
+                    await s_deviceClient.SendEventAsync(message);
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                    await Task.Delay(1000);
+                }
+     
+        }
+
+        private static async void ReceiveC2dAsync()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+                Message receivedMessage = await s_deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message: {0}",
+                Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                await s_deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("IoT Hub Quickstarts #1 - Simulated device. Ctrl-C to exit.\n");
+
+            // Connect to the IoT hub using the MQTT protocol
+            s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString, TransportType.Mqtt);
+            SendDeviceToCloudMessagesAsync();
+            ReceiveC2dAsync();
+            Console.ReadLine();
+        }
+    }
+}
